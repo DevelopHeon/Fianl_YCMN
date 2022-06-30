18781d4fa902bfb1a5b4f051b835b7cf539e6837
chkm306 <chkm306@gmail.com>
2022-06-30 15:01:09 +0900

    km - 예약 추가

--- FinalProject/src/main/java/com/uni/spring/reservation/model/service/ReservationService.java
+++ FinalProject/src/main/java/com/uni/spring/reservation/model/service/ReservationService.java
@@ -1,6 +1,7 @@
 package com.uni.spring.reservation.model.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.uni.spring.reservation.model.dto.Reservation;
 import com.uni.spring.reservation.model.dto.Resources;
@@ -18,4 +19,8 @@ public interface ReservationService {
 	void updateRsc(Resources rsc);
 
 	void deleteRsc(Resources rsc);
+
+	List<Reservation> findAll();
+
+	void insertReserve(Reservation rez);
 }
